c60de393e8a35ab4b044fb9368f96c806e254174
michael <m.deimel@miceportal.com>
2020-12-17 21:12:28 +0100

    add logs

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Podi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.autoloader = :classic
+    config.autoloader = :classic # error in production  define constant Devise::Mailer, but didn't (Zeitwerk::NameError)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
