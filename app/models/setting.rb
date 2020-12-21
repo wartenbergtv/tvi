9e66437ef3890b6d2386212fc8f2342cbf67fa54
michael <m.deimel@miceportal.com>
2020-12-21 16:30:46 +0100

    smaller pic

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -47,7 +47,7 @@ class Setting
   end
 
   def logo_url
-    "https://wartenberger-podcast.s3.eu-central-1.amazonaws.com/itunes-logo-2000x2000.png"
+    "https://wartenberger-podcast.s3.eu-central-1.amazonaws.com/itunes-logo-1400x1400.png"
   end
 
   # https://help.apple.com/itc/podcasts_connect/#/itc9267a2f12
