0b136d95322b697de252bf8a0cd87769945c916e
michael <m.deimel@miceportal.com>
2020-12-25 21:26:44 +0100

    upgrade

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :render404
 
   def not_found
     raise ActionController::RoutingError, "Not Found"
   end
 
-  def render_404
+  def render404
     respond_to do |format|
       format.html { render file: Rails.root.join("public/404.html"), layout: false, status: :not_found }
       format.xml  { head :not_found }
