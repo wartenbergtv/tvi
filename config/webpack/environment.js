726b2ef0e83d8e06728da89a17bc90dd741fbe81
michael <m.deimel@miceportal.com>
2021-02-16 17:49:17 +0100

    fix podlove player

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,19 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
+const CopyPlugin = require("copy-webpack-plugin");
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   'window.jQuery': 'jquery',
   Popper: ['popper.js', 'default']
+}))
+
+environment.plugins.append('CopyPlugin', new CopyPlugin({
+  patterns: [{
+    from: "node_modules/@podlove/web-player",
+    to: "media/player"
+  }],
 }))
 
 module.exports = environment
