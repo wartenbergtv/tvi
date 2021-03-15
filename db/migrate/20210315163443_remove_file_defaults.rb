25f2b20e6957ee0b792f494f18ed2245fba0a87e
michael <m.deimel@miceportal.com>
2021-03-15 18:29:30 +0100

    Update 20210315163443_remove_file_defaults.rb

--- db/migrate/20210315163443_remove_file_defaults.rb
+++ db/migrate/20210315163443_remove_file_defaults.rb
@@ -1,6 +1,6 @@
 class RemoveFileDefaults < ActiveRecord::Migration[6.1]
   def change
-    change_column_null(:episodes, :file_size,   true)
+    change_column_null(:episodes, :file_size, true)
     change_column_null(:episodes, :file_duration, true)
 
     remove_index :episodes, :file_url
