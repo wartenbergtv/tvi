16875c20158dbf4fefdb5707f27f993fef116ddc
michael <m.deimel@miceportal.com>
2021-03-18 09:59:34 +0100

    add purgeCSS for smaller packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-module.exports = {
+let environment = {
   plugins: [
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -7,6 +8,23 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
 }
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.haml',
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment
