6b72ed4eb0f8b772244046032611afe0447ca9f5
michael <m.deimel@miceportal.com>
2021-03-18 10:09:24 +0100

    Revert "add purgeCSS for smaller packages"
    This reverts commit 16875c20158dbf4fefdb5707f27f993fef116ddc.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
-let environment = {
+module.exports = {
   plugins: [
-    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -8,23 +7,6 @@ let environment = {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
+    })
   ]
 }
-
-// Only run PurgeCSS in production (you can also add staging here)
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/*.html.haml',
-        './app/**/*.html.erb',
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js'
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
-
-module.exports = environment
