c289f81e7c97d7afee54f64b54f54dd0682421a7
michael <m.deimel@miceportal.com>
2021-03-23 17:24:35 +0100

    revert

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,9 +22,6 @@ require "rspec/rails"
 #
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
-Cloudinary.config do |config|
-  config.cloud_name = "sample"
-end
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -64,11 +61,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.after(:all) do
-    # cleanup after tests
-    if Rails.env.test?
-      FileUtils.rm_rf(Dir["#{Rails.root}/tmp/storage"])
-    end
-  end
 end
